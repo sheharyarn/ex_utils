eb4ab6803bd7b657973c193679805ee23230aebe
Sheharyar Naseer <hello@sheharyar.me>
2016-10-13 00:44:19 +0500

    Added documentation for Map.atomize_keys

--- lib/ex_utils/map.ex
+++ lib/ex_utils/map.ex
@@ -1,9 +1,36 @@
 defmodule ExUtils.Map do
+  @moduledoc """
+  Elixir Utility methods for the `Map` module
+  """
+
 
   @default_options [recursive: true]
+  @doc """
+  Converts all (string) map keys to atoms
 
+  By default converts all string keys of the map and all sub-maps
+  to atoms. Conversion to atoms can be limited to the base map
+  by specifiying `recursive: false` in options
+
+  You can also call `:symbolize_keys/2` instead of `:atomize_keys/2`
+
+  ## Options
+
+  Only accepts one option: `recursive` with default value `true`
+
+  ## Examples
+
+  ```
+  map = %{"a" => 1, "b" => %{"c" => 3, "d" => 4} }
+
+  ExUtils.Map.symbolize_keys(map)
+  #=> %{a: 1, b: %{c: 3, d: 4}}
+
+  ExUtils.Map.atomize_keys(map, recursive: false)
+  #=> %{a: 1, b: %{"c" => 3, "d" => 4}}
+  ```
+  """
   def atomize_keys(map, opts \\ [])
-
   def atomize_keys(map, [recursive: false]) do
     Enum.reduce map, %{}, fn {k, v}, m ->
       map_put(m, k, v)
@@ -25,6 +52,13 @@ defmodule ExUtils.Map do
     end
   end
 
+
+  defdelegate symbolize_keys(map, opts \\ []), to: __MODULE__, as: :atomize_keys
+
+
+
+  ## Private Methods
+
   defp map_put(m, k, v) do
     cond do
       is_binary(k) -> Map.put(m, String.to_atom(k), v)
@@ -32,5 +66,4 @@ defmodule ExUtils.Map do
     end
   end
 
-  defdelegate symbolize_keys(map, opts \\ []), to: __MODULE__, as: :atomize_keys
 end
