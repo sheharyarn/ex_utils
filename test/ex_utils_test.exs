04e450326ddd2e8b829ed896328d522eb1c3fbbd
Sheharyar Naseer <hello@sheharyar.me>
2016-10-12 05:09:34 +0500

    Getting tests to pass for :is_struct? and :is_pure_map?

---
+++ test/ex_utils_test.exs
@@ -0,0 +1,21 @@
+defmodule ExUtils.Tests.Core do
+  use ExUnit.Case
+
+  defmodule Person do
+    defstruct [:name, :age]
+  end
+
+  setup do
+    [map: %{a: 1, b: 2, c: 3}, struct: %Person{name: "Ali", age: 23}]
+  end
+
+  test "is_struct? returns true only for structs", context do
+    assert ExUtils.is_struct?(context[:struct])
+    refute ExUtils.is_struct?(context[:map])
+  end
+
+  test "is_pure_map? returns true only for maps", context do
+    assert ExUtils.is_pure_map?(context[:map])
+    refute ExUtils.is_pure_map?(context[:struct])
+  end
+end
