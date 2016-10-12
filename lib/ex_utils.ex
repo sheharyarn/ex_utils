aaa0466e1ccb6a02995fe4b147b0aaa52c2bf2d0
Sheharyar Naseer <hello@sheharyar.me>
2016-10-12 05:09:12 +0500

    Added two new methods - :is_struct? and :is_pure_map?

--- lib/ex_utils.ex
+++ lib/ex_utils.ex
@@ -1,2 +1,8 @@
 defmodule ExUtils do
+  def is_struct?(%{__struct__: _}), do: true
+  def is_struct?(_),                do: false
+
+  def is_pure_map?(term) do
+    is_map(term) && !is_struct?(term)
+  end
 end
