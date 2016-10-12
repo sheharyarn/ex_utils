d83853a6e88dd1f6fa0813eeb9af33d530016c62
Sheharyar Naseer <hello@sheharyar.me>
2016-10-13 00:44:05 +0500

    Added documentation for :is_struct? and :is_pure_map?

--- lib/ex_utils.ex
+++ lib/ex_utils.ex
@@ -1,8 +1,38 @@
 defmodule ExUtils do
+  @moduledoc """
+  Collection of core Elixir utility methods that don't fit
+  in any specific Module
+  """
+
+
+  @doc """
+  Checks if the term is a struct as well as a map
+
+  ## Example
+
+  ```
+  ExUnit.is_struct?(:atom)                  # => false
+  ExUnit.is_struct?(%{a: 1})                # => false
+  ExUnit.is_struct?(%Person{name: "Ali"})   # => true
+  ```
+  """
+  def is_struct?(term)
   def is_struct?(%{__struct__: _}), do: true
   def is_struct?(_),                do: false
 
+
+  @doc """
+  Checks if the term is a pure map (i.e. not a struct)
+
+  ## Example
+
+  ```
+  ExUtils.is_pure_map?(%{a: 1, b: 2})          # => true
+  ExUtils.is_pure_map?(%Person{name: "Ali"})   # => false
+  ```
+  """
   def is_pure_map?(term) do
     is_map(term) && !is_struct?(term)
   end
 end
+
