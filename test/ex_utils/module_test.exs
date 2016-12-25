5378ea43e8af6e394d2d5d6db1f07bed68a53253
Sheharyar Naseer <hello@sheharyar.me>
2016-12-25 20:24:52 +0500

    Test for Module#name

--- test/ex_utils/module_test.exs
+++ test/ex_utils/module_test.exs
@@ -7,7 +7,7 @@ defmodule ExUtils.Tests.Module do
     [struct: %Person{name: "Ali", age: 23}]
   end
 
-  test "methods returns a list of all methods of a module" do
+  test "#methods returns a list of all methods of a module" do
     animal_methods = ExUtils.Module.methods(Animal)
     person_methods = ExUtils.Module.methods(Person)
 
@@ -18,7 +18,7 @@ defmodule ExUtils.Tests.Module do
     assert Enum.member?(person_methods, {:old?, 2})
   end
 
-  test "has_method? returns true if method exists for module" do
+  test "#has_method? returns true if method exists for module" do
     assert ExUtils.Module.has_method?(Person, :old?)      == true
     assert ExUtils.Module.has_method?(Person, {:old?, 1}) == true
     assert ExUtils.Module.has_method?(Person, {:old?, 2}) == true
@@ -26,5 +26,10 @@ defmodule ExUtils.Tests.Module do
     assert ExUtils.Module.has_method?(Person, :unknown)   == false
     assert ExUtils.Module.has_method?(Person, {:old?, 0}) == false
     assert ExUtils.Module.has_method?(Person, {:old?, 3}) == false
+  end
+
+  test "#name returns Module name without Elixir namespace" do
+    assert ExUtils.Module.name(SomeModule)     == "SomeModule"
+    assert ExUtils.Module.name(Another.Module) == "Another.Module"
   end
 end
