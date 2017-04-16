cb68476f9e3f1059f3f9657d45172f33ba436e4f
Sheharyar Naseer <hello@sheharyar.me>
2017-04-15 20:14:33 -0400

    Bumped version to v0.1.6

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExUtils.Mixfile do
 
   @app     :ex_utils
   @name    "ExUtils"
-  @version "0.1.5"
+  @version "0.1.6"
   @github  "https://github.com/sheharyarn/#{@app}"
 
   def project do
